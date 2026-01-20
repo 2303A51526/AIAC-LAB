31cc87618696fb578285be08df955e6dd2fcedd0
sai teja <2303A51526@sru.edu.in>
2026-01-20 14:44:26 +0530

    Delete task-2.py

--- task-2.py
+++
@@ -1,6 +0,0 @@
-#Refactor the code to eliminate extra variables, streamline the loop, and enhance overall clarity.
-number = int(input("Enter a number to find its factorial: "))
-factorial = 1
-for i in range(2, number + 1):
-    factorial *= i  
-print(f"The factorial of {number} is {factorial}")
