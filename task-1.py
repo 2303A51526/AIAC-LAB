1b31987e44e770b9dc65cd18784d52a483d2426e
sai teja <2303A51526@sru.edu.in>
2026-01-20 14:43:51 +0530

    Delete task-1.py

--- task-1.py
+++
@@ -1,6 +0,0 @@
-#Write a Python program that finds the factorial of a number without defining any functions.
-number = int(input("Enter a number to find its factorial: "))
-factorial = 1
-for i in range(1, number + 1):
-    factorial *= i
-print(f"The factorial of {number} is {factorial}")
